b29c37c5a8ac5a77e9f895db20b9f58c3ce3f3aa
Abhishek <controlblaster02@gmail.com>
2024-12-26 22:42:14 +0530

    updated evaluation strategies file in src and evalation in steps

---
+++ src/evaluation.py
@@ -0,0 +1,63 @@
+import logging
+from abc import ABC,abstractmethod
+
+import numpy as np
+from sklearn.metrics import mean_squared_error,r2_score,root_mean_squared_error
+
+class Evauation(ABC):
+    """
+    Abstract class defining strategy for evaluation our models
+    """
+    @abstractmethod
+    def calculate_scores(self, y_true:np.ndarry, y_pred: np.ndarray):
+        """
+        Calculates the scores for the model
+        Args:
+            y_true: True labels
+            y_pred: Predicted labels
+        Returns:
+            None
+        """
+        pass
+
+class MSE(Evauation):
+    """
+    Evaluation Strategy that used Mean Squared Error
+    """
+    def calculate_scores(self, y_true, y_pred):
+        try:
+            logging.info("Calculation info")
+            mse= mean_squared_error(y_true,y_pred)
+            logging.info("MSE : {}".format(mse))
+            return mse
+        except Exception as e:
+            logging.info("Error in calculation MSE: {}".format(e))
+            raise e
+
+class R2(Evauation):
+    """
+    Evaluation Strategy that used R2 Score
+    """
+    def calculate_scores(self, y_true, y_pred):
+        try:
+            logging.info("Calculation info")
+            r2= r2_score(y_true,y_pred)
+            logging.info("R2 Score : {}".format(r2))
+            return r2
+        except Exception as e:
+            logging.info("Error in calculation R2 Score: {}".format(e))
+            raise e
+
+class RMSE(Evauation):
+    """
+    Evaluation Strategy that used root mean squared
+    """
+    def calculate_scores(self, y_true, y_pred):
+        try:
+            logging.info("Calculation info")
+            rmse= root_mean_squared_error(y_true,y_pred)
+            logging.info("RMSE : {}".format(rmse))
+            return rmse
+        except Exception as e:
+            logging.info("Error in calculation RMSE: {}".format(e))
+            raise e
--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,13 +1,35 @@
 import logging
 import pandas as pd
 from zenml import step
+from src.evaluation import R2,RMSE,MSE
+from sklearn.base import RegressorMixin
+from typing import Tuple
+from typing_extensions import Annotated
 
 @step
-def evaluate_model(df: pd.DataFrame)->None:
+def evaluate_model(model: RegressorMixin,
+                   X_test:pd.DataFrame,
+                   y_test:pd.Series)->Tuple[
+                       Annotated[float,"r2 score"],
+                       Annotated[float,"rmse"]
+                   ]:
     """
     Evaluates the model on the ingested data.
     Args:
         df: the Ingested data
     """
+    try:
+        prediction = model.predict(X_test)
+        mse_class=MSE()
+        mse=mse_class(y_test,prediction)
 
-    pass
+        r2_class=R2()
+        r2=r2_class(y_test,prediction)
+
+        rmse_class=RMSE()
+        rmse=rmse_class(y_test,prediction)
+
+        return r2,rmse
+    except Exception as e:
+        logging.error("Error in Evaluating model: {}".format(e))
+        raise e
