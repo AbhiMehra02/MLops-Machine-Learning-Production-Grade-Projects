c4de51fc4e6c79dcf3deb2fb67309a48deb9063b
Abhishek <controlblaster02@gmail.com>
2024-12-26 18:50:05 +0530

    creating model devlopment

---
+++ src/model_dev.py
@@ -0,0 +1,49 @@
+import logging 
+from abc import ABC, abstractmethod
+
+from sklearn.linear_model import LinearRegression
+
+class Model(ABC):
+    """
+    Abstract class for all models
+    """
+
+    @abstractmethod
+    def train(self,X_train,y_train):
+        """
+        Training the model
+        Args:
+            X_train : Training data
+            y_train : Training labels
+        
+        Returns:
+            None
+        """
+        pass
+
+class LinearRegression(Model):
+    """
+    Linear Regression model
+    """
+    
+    def train(self,X_train,y_train):
+        """
+        Training the model
+        Args:
+            X_train : Training data
+            y_train : Training labels
+        
+        Returns:
+            None
+        """
+        try:
+            reg = LinearRegression()
+            reg.model.fit(X_train,y_train)
+            logging.info("Model training completed")
+            return reg
+        except Exception as e:
+            logging.error("Error in training model: {}".format(e))
+            raise e 
+    
+    
+
