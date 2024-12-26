cddcbcc5296cd6e0cde33aa5ffc628032c20f0ae
Abhishek <controlblaster02@gmail.com>
2024-12-26 22:18:25 +0530

    model_train and configuration of model added

---
+++ steps/config.py
@@ -0,0 +1,8 @@
+from zenml.steps import BaseParameters
+
+class ModelNameConfig(BaseParameters):
+    """
+    Model Config
+    """
+    model_name: str = "LinearRegression"
+    model_kwargs: dict ={}
